4d33c332087eb61c88e3fbe8c8c6012c7c6b4184
Matheus <matheus.pgm2000@gmail.com>
2022-02-21 03:48:31 -0300

    feat: finish crud

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('produtos')
 export class ProdutosController {
   @Get()
   obterTodos(): string {
     return 'Lista todos os produtos';
+  }
+  @Get(':id')
+  obterUm(@Param() params): string {
+    return `Retornar os dados do produto ${params.id}`;
+  }
+  @Post('')
+  criar(@Body() produto): string {
+    console.log(produto);
+    return 'Produto criado';
+  }
+  @Put('')
+  alterar(@Body() produto): string {
+    console.log(produto);
+    return 'Produto atualizado';
+  }
+  @Delete(':id')
+  apagar(@Param() params): string {
+    return `Apagar o produto ${params.id}`;
   }
 }
